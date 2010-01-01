ee4ce32ea8e09222cefadca7d2d37f6001f4a6b0
Dave Gamble <davegamble@gmail.com>
2010-01-01 23:13:31 +0000

    make strcasecmp STABLE if passed a null string
    git-svn-id: http://svn.code.sf.net/p/cjson/code@27 e3330c51-1366-4df0-8b21-3ccf24e3d50e

--- cJSON.c
+++ cJSON.c
@@ -33,6 +33,7 @@
 
 static int cJSON_strcasecmp(const char *s1,const char *s2)
 {
+	if (!s1) return (s1==s2)?0:1;if (!s2) return 1;
 	for(; tolower(*s1) == tolower(*s2); ++s1, ++s2)	if(*s1 == 0)	return 0;
 	return tolower(*(const unsigned char *)s1) - tolower(*(const unsigned char *)s2);
 }
