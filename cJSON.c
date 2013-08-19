28691956a607ccec14b1f31756c7f179d50a0d17
Dave Gamble <davegamble@gmail.com>
2013-08-19 14:49:06 +0000

    fix \u parser
    git-svn-id: http://svn.code.sf.net/p/cjson/code@58 e3330c51-1366-4df0-8b21-3ccf24e3d50e

--- cJSON.c
+++ cJSON.c
@@ -141,11 +141,11 @@ static unsigned parse_hex4(const char *str)
 {
 	unsigned h=0;
 	if (*str>='0' && *str<='9') h+=(*str)-'0'; else if (*str>='A' && *str<='F') h+=10+(*str)-'A'; else if (*str>='a' && *str<='f') h+=10+(*str)-'a'; else return 0;
-	h=(h&15)<<4;str++;
+	h=h<<4;str++;
 	if (*str>='0' && *str<='9') h+=(*str)-'0'; else if (*str>='A' && *str<='F') h+=10+(*str)-'A'; else if (*str>='a' && *str<='f') h+=10+(*str)-'a'; else return 0;
-	h=(h&15)<<4;str++;
+	h=h<<4;str++;
 	if (*str>='0' && *str<='9') h+=(*str)-'0'; else if (*str>='A' && *str<='F') h+=10+(*str)-'A'; else if (*str>='a' && *str<='f') h+=10+(*str)-'a'; else return 0;
-	h=(h&15)<<4;str++;
+	h=h<<4;str++;
 	if (*str>='0' && *str<='9') h+=(*str)-'0'; else if (*str>='A' && *str<='F') h+=10+(*str)-'A'; else if (*str>='a' && *str<='f') h+=10+(*str)-'a'; else return 0;
 	return h;
 }
