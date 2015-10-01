8865d869db2367a6d48459eea739ebb8369eb1b7
Kevin Branigan <kbranigan@gmail.com>
2015-10-01 12:03:27 -0400

    cJSON_HasObjectItem didn’t compile

--- cJSON.h
+++ cJSON.h
@@ -80,7 +80,7 @@ extern int	  cJSON_GetArraySize(cJSON *array);
 extern cJSON *cJSON_GetArrayItem(cJSON *array,int item);
 /* Get item "string" from object. Case insensitive. */
 extern cJSON *cJSON_GetObjectItem(cJSON *object,const char *string);
-extern int *cJSON_HasObjectItem(cJSON *object,const char *string);
+extern int cJSON_HasObjectItem(cJSON *object,const char *string);
 /* For analysing failed parses. This returns a pointer to the parse error. You'll probably need to look a few chars back to make sense of it. Defined when cJSON_Parse() returns 0. 0 when cJSON_Parse() succeeds. */
 extern const char *cJSON_GetErrorPtr(void);
 	
