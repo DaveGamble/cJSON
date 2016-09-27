d5bd49763679b5c164247b16a4806278c5b8f688
Max Bruckner <max@maxbruckner.de>
2016-09-27 23:44:47 +0700

    reformatting: pow2gt

--- cJSON.c
+++ cJSON.c
@@ -199,7 +199,19 @@ static const char *parse_number(cJSON *item, const char *num)
     return num;
 }
 
-static int pow2gt (int x)	{	--x;	x|=x>>1;	x|=x>>2;	x|=x>>4;	x|=x>>8;	x|=x>>16;	return x+1;	}
+/* calculate the next largest power of 2 */
+static int pow2gt (int x)
+{
+    --x;
+
+    x |= x >> 1;
+    x |= x >> 2;
+    x |= x >> 4;
+    x |= x >> 8;
+    x |= x >> 16;
+
+    return x + 1;
+}
 
 typedef struct {char *buffer; int length; int offset; } printbuffer;
 
