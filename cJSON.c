5533d23382ea6184c6bdb844601c3c7add736315
viker <viker@viker.cz>
2016-09-27 16:53:02 +0200

    2 functions from bottom readable

--- cJSON.c
+++ cJSON.c
@@ -711,16 +711,32 @@ cJSON *cJSON_CreateStringArray(const char **strings,int count)	{int i;cJSON *n=0
 /* Duplication */
 cJSON *cJSON_Duplicate(cJSON *item,int recurse)
 {
-	cJSON *newitem,*cptr,*nptr=0,*newchild;
+	cJSON *newitem, *cptr, *nptr=0, *newchild;
 	/* Bail on bad ptr */
 	if (!item) return 0;
 	/* Create new item */
 	newitem=cJSON_New_Item();
 	if (!newitem) return 0;
 	/* Copy over all vars */
-	newitem->type=item->type&(~cJSON_IsReference),newitem->valueint=item->valueint,newitem->valuedouble=item->valuedouble;
-	if (item->valuestring)	{newitem->valuestring=cJSON_strdup(item->valuestring);	if (!newitem->valuestring)	{cJSON_Delete(newitem);return 0;}}
-	if (item->string)		{newitem->string=cJSON_strdup(item->string);			if (!newitem->string)		{cJSON_Delete(newitem);return 0;}}
+	newitem->type = item->type&(~cJSON_IsReference), newitem->valueint = item->valueint, newitem->valuedouble = item->valuedouble;
+	if (item->valuestring)	
+	{
+		newitem->valuestring=cJSON_strdup(item->valuestring);	
+		if (!newitem->valuestring)	
+		{
+			cJSON_Delete(newitem);
+			return 0;
+		}
+	}
+	if (item->string)		
+	{
+		newitem->string=cJSON_strdup(item->string);			
+		if (!newitem->string)		
+		{
+			cJSON_Delete(newitem);
+			return 0;
+		}
+	}
 	/* If non-recursive, then we're done! */
 	if (!recurse) return newitem;
 	/* Walk the ->next chain for the child. */
@@ -728,9 +744,21 @@ cJSON *cJSON_Duplicate(cJSON *item,int recurse)
 	while (cptr)
 	{
 		newchild=cJSON_Duplicate(cptr,1);		/* Duplicate (with recurse) each item in the ->next chain */
-		if (!newchild) {cJSON_Delete(newitem);return 0;}
-		if (nptr)	{nptr->next=newchild,newchild->prev=nptr;nptr=newchild;}	/* If newitem->child already set, then crosswire ->prev and ->next and move on */
-		else		{newitem->child=newchild;nptr=newchild;}					/* Set newitem->child and move to it */
+		if (!newchild) 
+		{
+			cJSON_Delete(newitem);
+			return 0;
+		}
+		if (nptr)	
+		{
+			nptr->next = newchild, newchild->prev = nptr;
+			nptr=newchild;
+		}	/* If newitem->child already set, then crosswire ->prev and ->next and move on */
+		else		
+		{
+			newitem->child=newchild;
+			nptr=newchild;
+		}					/* Set newitem->child and move to it */
 		cptr=cptr->next;
 	}
 	return newitem;
