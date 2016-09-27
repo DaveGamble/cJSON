de0e1a79f51d6321ba6c285ec957b6e3dc24e889
viker <viker@viker.cz>
2016-09-27 16:49:44 +0200

    last function more readable

--- cJSON.c
+++ cJSON.c
@@ -745,10 +745,25 @@ void cJSON_Minify(char *json)
 		else if (*json=='\t') json++;	/* Whitespace characters. */
 		else if (*json=='\r') json++;
 		else if (*json=='\n') json++;
-		else if (*json=='/' && json[1]=='/')  while (*json && *json!='\n') json++;	/* double-slash comments, to end of line. */
-		else if (*json=='/' && json[1]=='*') {while (*json && !(*json=='*' && json[1]=='/')) json++;json+=2;}	/* multiline comments. */
-		else if (*json=='\"'){*into++=*json++;while (*json && *json!='\"'){if (*json=='\\') *into++=*json++;*into++=*json++;}*into++=*json++;} /* string literals, which are \" sensitive. */
-		else *into++=*json++;			/* All other characters. */
+		else if (*json=='/' && json[1]=='/')  
+			while (*json && *json!='\n') json++;	/* double-slash comments, to end of line. */
+		else if (*json=='/' && json[1]=='*') 
+		{
+			while (*json && !(*json=='*' && json[1]=='/')) json++;
+			json+=2;
+		}	/* multiline comments. */
+		else if (*json=='\"')
+		{
+			*into++=*json++;
+			while (*json && *json!='\"')
+			{
+				if (*json=='\\') 
+					*into++=*json++;
+					*into++=*json++;
+			}
+			*into++=*json++;
+		} /* string literals, which are \" sensitive. */
+		else *into++ = *json++;			/* All other characters. */
 	}
 	*into=0;	/* and null-terminate. */
 }
