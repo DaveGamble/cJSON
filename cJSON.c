8db7e6d320f336367fb94d6b7133cd2e94816b63
Max Bruckner <max@maxbruckner.de>
2016-09-27 23:39:54 +0700

    reformatting: cJSON_InitHooks

--- cJSON.c
+++ cJSON.c
@@ -81,14 +81,16 @@ static char* cJSON_strdup(const char* str)
 
 void cJSON_InitHooks(cJSON_Hooks* hooks)
 {
-    if (!hooks) { /* Reset hooks */
+    if (!hooks)
+    {
+        /* Reset hooks */
         cJSON_malloc = malloc;
         cJSON_free = free;
         return;
     }
 
-	cJSON_malloc = (hooks->malloc_fn)?hooks->malloc_fn:malloc;
-	cJSON_free	 = (hooks->free_fn)?hooks->free_fn:free;
+    cJSON_malloc = (hooks->malloc_fn) ? hooks->malloc_fn : malloc;
+    cJSON_free = (hooks->free_fn) ? hooks->free_fn : free;
 }
 
 /* Internal constructor. */
