55488f66f60269d309b7d1a0620bdf8c4dfbf7d9
Max Bruckner <max@maxbruckner.de>
2016-09-27 23:40:16 +0700

    reformatting: cJSON_New_Item

--- cJSON.c
+++ cJSON.c
@@ -96,9 +96,13 @@ void cJSON_InitHooks(cJSON_Hooks* hooks)
 /* Internal constructor. */
 static cJSON *cJSON_New_Item(void)
 {
-	cJSON* node = (cJSON*)cJSON_malloc(sizeof(cJSON));
-	if (node) memset(node,0,sizeof(cJSON));
-	return node;
+    cJSON* node = (cJSON*)cJSON_malloc(sizeof(cJSON));
+    if (node)
+    {
+        memset(node, 0, sizeof(cJSON));
+    }
+
+    return node;
 }
 
 /* Delete a cJSON structure. */
