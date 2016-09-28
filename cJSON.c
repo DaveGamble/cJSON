9b7eb6a46db2a345726753290b4a48db3b06b9e3
Max Bruckner <max@maxbruckner.de>
2016-09-28 20:43:46 +0700

    reformatting: cJSON_PrintBuffered

--- cJSON.c
+++ cJSON.c
@@ -841,13 +841,14 @@ char *cJSON_PrintUnformatted(cJSON *item)
     return print_value(item, 0, 0, 0);
 }
 
-char *cJSON_PrintBuffered(cJSON *item,int prebuffer,int fmt)
+char *cJSON_PrintBuffered(cJSON *item, int prebuffer, int fmt)
 {
-	printbuffer p;
-	p.buffer=(char*)cJSON_malloc(prebuffer);
-	p.length=prebuffer;
-	p.offset=0;
-	return print_value(item,0,fmt,&p);
+    printbuffer p;
+    p.buffer = (char*)cJSON_malloc(prebuffer);
+    p.length = prebuffer;
+    p.offset = 0;
+
+    return print_value(item, 0, fmt, &p);
 }
 
 
