ae4681b7871d2cc7fb159c67f5b1af3d887a5e3b
Max Bruckner <max@maxbruckner.de>
2017-02-18 02:46:36 +0100

    fuzzing: use llvm source code instrumentation

--- fuzzing/afl.sh
+++ fuzzing/afl.sh
@@ -5,5 +5,5 @@ cd afl-build || exit 1
 #cleanup
 rm -r -- *
 
-CC=afl-gcc cmake ../.. -DENABLE_FUZZING=On -DENABLE_SANITIZERS=On -DBUILD_SHARED_LIBS=Off
+CC=afl-clang-fast cmake ../.. -DENABLE_FUZZING=On -DENABLE_SANITIZERS=On -DBUILD_SHARED_LIBS=Off
 make afl
