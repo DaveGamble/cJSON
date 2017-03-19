b7ce06224bb6a1ad22dece90ac5ca530eef1685c
Max Bruckner <max@maxbruckner.de>
2017-03-19 11:05:33 +0100

    Release version 1.4.3

--- cJSON.h
+++ cJSON.h
@@ -31,7 +31,7 @@ extern "C"
 /* project version */
 #define CJSON_VERSION_MAJOR 1
 #define CJSON_VERSION_MINOR 4
-#define CJSON_VERSION_PATCH 2
+#define CJSON_VERSION_PATCH 3
 
 #include <stddef.h>
 
