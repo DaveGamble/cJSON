bde95a1b0fba7d5ccb31ee26e5d1c8b5f16d265f
Max Bruckner <max@maxbruckner.de>
2017-03-22 12:46:42 +0100

    Update copyright

--- cJSON.h
+++ cJSON.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2009 Dave Gamble
+  Copyright (c) 2009-2017 Dave Gamble and cJSON contributors
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
