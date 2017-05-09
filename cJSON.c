5f1fdb4c6fa5a21b2e36cd2871c3ae90421f49f7
Max Bruckner <max@maxbruckner.de>
2017-05-10 01:17:36 +0200

    Add warning -Wunused-macro

--- cJSON.c
+++ cJSON.c
@@ -208,7 +208,6 @@ typedef struct
 
 /* check if the given size is left to read in a given parse buffer (starting with 1) */
 #define can_read(buffer, size) ((buffer != NULL) && (((buffer)->offset + size) <= (buffer)->length))
-#define cannot_read(buffer, size) (!can_read(buffer, size))
 /* check if the buffer can be accessed at the given index (starting with 0) */
 #define can_access_at_index(buffer, index) ((buffer != NULL) && (((buffer)->offset + index) < (buffer)->length))
 #define cannot_access_at_index(buffer, index) (!can_access_at_index(buffer, index))
