ce5f31ac47ce4bbb84ed2c4ee12da303c78687b3
Max Bruckner <max@maxbruckner.de>
2018-02-03 19:14:19 +0100

    Remove superfluous null checks in can_read/access_at_index macros

--- cJSON.c
+++ cJSON.c
@@ -270,9 +270,9 @@ typedef struct
 } parse_buffer;
 
 /* check if the given size is left to read in a given parse buffer (starting with 1) */
-#define can_read(buffer, size) ((buffer != NULL) && (((buffer)->offset + size) <= (buffer)->length))
+#define can_read(buffer, size) (((buffer)->offset + (size)) <= (buffer)->length)
 /* check if the buffer can be accessed at the given index (starting with 0) */
-#define can_access_at_index(buffer, index) ((buffer != NULL) && (((buffer)->offset + index) < (buffer)->length))
+#define can_access_at_index(buffer, index) (((buffer)->offset + (index)) < (buffer)->length)
 #define cannot_access_at_index(buffer, index) (!can_access_at_index(buffer, index))
 /* get a pointer to the buffer at the position */
 #define buffer_at_offset(buffer) ((buffer)->content + (buffer)->offset)
