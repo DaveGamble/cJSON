155281f235d87240ffd7d187f52ff26ae228f95f
Max Bruckner <max@maxbruckner.de>
2018-06-24 21:21:46 +0200

    Unity: Set UNITY_EXCLUDE_STDINT_H to disable the use of long long

--- tests/unity/examples/unity_config.h
+++ tests/unity/examples/unity_config.h
@@ -63,7 +63,7 @@
  * you're going to want to define this. That way, Unity will know to skip the
  * inclusion of this file and you won't be left with a compiler error.
  */
-/* #define UNITY_EXCLUDE_STDINT_H */
+#define UNITY_EXCLUDE_STDINT_H
 
 /* ********************** MANUAL INTEGER TYPE DEFINITION ***********************
  * If you've disabled all of the automatic options above, you're going to have
