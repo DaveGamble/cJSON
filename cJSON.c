7cb9a2482221a2e40f4fe16c52b5156a1752cd41
Kadir Eken <47029063+kadireken@users.noreply.github.com>
2020-10-12 14:29:16 +0300

    Update cJSON.c
    In the line 2906, if the item is null then cJSON_IsNull(item) function returns false. I reckon it should return true, otherwise my test program throws segmentation fault in order to access null object.

--- cJSON.c
+++ cJSON.c
@@ -2903,7 +2903,7 @@ CJSON_PUBLIC(cJSON_bool) cJSON_IsNull(const cJSON * const item)
 {
     if (item == NULL)
     {
-        return false;
+        return true;
     }
 
     return (item->type & 0xFF) == cJSON_NULL;
