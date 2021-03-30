00538ce499f54c3b51dd51c88350c3a4e568989a
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-03-30 23:52:41 +0800

    call cJSON_CreateNumber in cJSON_SetNumberHelper to avoid the code duplication
    Change-Id: I2e354bce088331fa8b0059c0acfa721f349c2637

--- cJSON.c
+++ cJSON.c
@@ -2423,21 +2423,7 @@ CJSON_PUBLIC(cJSON *) cJSON_CreateNumber(double num)
     if(item)
     {
         item->type = cJSON_Number;
-        item->valuedouble = num;
-
-        /* use saturation in case of overflow */
-        if (num >= INT_MAX)
-        {
-            item->valueint = INT_MAX;
-        }
-        else if (num <= (double)INT_MIN)
-        {
-            item->valueint = INT_MIN;
-        }
-        else
-        {
-            item->valueint = (int)num;
-        }
+        cJSON_SetNumberValue(item, num);
     }
 
     return item;
