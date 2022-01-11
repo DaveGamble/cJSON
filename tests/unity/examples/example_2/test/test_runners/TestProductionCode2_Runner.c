b3a9bef9b8aff0519d2fd4c50b29116395c43856
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-11 13:37:13 +0800

    Delete tests directory

--- tests/unity/examples/example_2/test/test_runners/TestProductionCode2_Runner.c
+++
@@ -1,9 +0,0 @@
-#include "unity.h"
-#include "unity_fixture.h"
-
-TEST_GROUP_RUNNER(ProductionCode2)
-{
-  RUN_TEST_CASE(ProductionCode2, IgnoredTest);
-  RUN_TEST_CASE(ProductionCode2, AnotherIgnoredTest);
-  RUN_TEST_CASE(ProductionCode2, ThisFunctionHasNotBeenTested_NeedsToBeImplemented);
-}
--- tests/unity_setup.c
+++
@@ -1,3 +0,0 @@
-// msvc doesn't support weak-linking, so we need to define these functions.
-void setUp(void) { }
-void tearDown(void) { }
