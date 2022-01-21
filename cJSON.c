b64963332f8e5cdd7d65a645b21e316e394ab169
“uinontech_yuxiaojun” <yuxj@localhost.localdomain>
2022-01-21 09:47:56 +0800

    fix:print_number() print incorrect integer

--- cJSON.c
+++ cJSON.c
@@ -564,15 +564,22 @@ static cJSON_bool print_number(const cJSON * const item, printbuffer * const out
     }
     else
     {
-        /* Try 15 decimal places of precision to avoid nonsignificant nonzero digits */
-        length = sprintf((char*)number_buffer, "%1.15g", d);
+	    if(item->valuedouble == item->valueint)
+	    {
+		    length = sprintf((char*)number_buffer, "%d", item->valueint);
+	    }
+	    else
+	    {
+		    /* Try 15 decimal places of precision to avoid nonsignificant nonzero digits */
+		    length = sprintf((char*)number_buffer, "%1.15g", d);
 
-        /* Check whether the original double can be recovered */
-        if ((sscanf((char*)number_buffer, "%lg", &test) != 1) || !compare_double((double)test, d))
-        {
-            /* If not, print with 17 decimal places of precision */
-            length = sprintf((char*)number_buffer, "%1.17g", d);
-        }
+		    /* Check whether the original double can be recovered */
+		    if ((sscanf((char*)number_buffer, "%lg", &test) != 1) || !compare_double((double)test, d))
+		    {
+			    /* If not, print with 17 decimal places of precision */
+			    length = sprintf((char*)number_buffer, "%1.17g", d);
+		    }
+	    }
     }
 
     /* sprintf failed or buffer overrun occurred */
