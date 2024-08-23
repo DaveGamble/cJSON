67d170616be49c4204605802d63c2b4c9dab8c3b
Nicolas Badoux <n.badoux@hotmail.com>
2024-08-23 14:14:10 +0000

    Check for NULL in cJSON_DetachItemViaPointer

--- cJSON.c
+++ cJSON.c
@@ -2204,7 +2204,7 @@ CJSON_PUBLIC(cJSON*) cJSON_AddArrayToObject(cJSON * const object, const char * c
 
 CJSON_PUBLIC(cJSON *) cJSON_DetachItemViaPointer(cJSON *parent, cJSON * const item)
 {
-    if ((parent == NULL) || (item == NULL))
+    if ((parent == NULL) || (parent->child == NULL) || (item == NULL) || (item->prev == NULL))
     {
         return NULL;
     }
